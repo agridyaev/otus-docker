0faabb5d2e49429f1272f256902d24928ca01e71
Andrey Gridyaev <agridyaev@gmail.com>
2022-01-16 17:28:03 +0000

    tests.sh: add --network selenoid and change container name

--- tests.sh
+++ tests.sh
@@ -2,12 +2,14 @@
 
 docker build -t tests .
 
-docker run --name test_run tests pytest --browser $1 -n $2
+docker run --name tests_run tests pytest --browser $1 -n $2
 
-docker cp test_run:/app/allure-report .
+docker cp tests_run:/app/allure-report .
 
 allure serve allure-report
 
-docker rm test_run
+docker rm tests_run
 
-# docker run --name test_run tests && docker cp test_run:/app/allure-report . && allure serve allure-report
+# docker run --name tests_run --network selenoid tests \
+# && docker cp tests_run:/app/allure-report . \
+# && allure serve allure-report
