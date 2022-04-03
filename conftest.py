78ae58e868c2d4f4b44d059991d4870261482056
Andrey Gridyaev <agridyaev@gmail.com>
2022-04-04 00:33:12 +0300

    conftest: bump chrome version

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
     parser.addoption("--api_url", default="https://jsonplaceholder.typicode.com")
     parser.addoption("--opencart_url", default="https://demo.opencart.com")
     parser.addoption("--browser", default="chrome")
-    parser.addoption("--browser_version", action="store", default="93.0")
+    parser.addoption("--browser_version", action="store", default="98.0")
     parser.addoption("--executor",
                      # имя контейнера в сети selenoid
                      default="selenoid",
