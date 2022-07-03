e5826686fca2e63512ea24bcf24244c80aac3e5d
Andrey Gridyaev <agridyaev@gmail.com>
2022-07-04 00:09:05 +0300

    tests.sh: add possibility to process failed tests

--- tests.sh
+++ tests.sh
@@ -11,5 +11,5 @@ allure serve allure-report
 docker rm tests_run
 
 # docker run --name tests_run --network selenoid tests \
-# && docker cp tests_run:/app/allure-report . \
+# ; docker cp tests_run:/app/allure-report . \
 # && allure serve allure-report
