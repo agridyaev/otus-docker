522696ed391ae55658a22f79283c6193223c1fb7
Andrey Gridyaev <agridyaev@gmail.com>
2022-12-11 23:55:49 +0300

    tests/test_ui: use DuckDuckGo instead of Google

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -3,16 +3,16 @@ import allure
 from framework import verify_element_presence
 
 
-@allure.story("Google")
-def test_google(browser):
-    browser.open_page("https://google.ru")
-    verify_element_presence(browser, "button[class='close-button']")
-    verify_element_presence(browser, "input[value='Поиск в Google']")
-    browser.check_in_title("Google")
+@allure.story("DuckDuckGo")
+def test_duckduckgo(browser):
+    browser.open_page("https://duckduckgo.com/")
+    verify_element_presence(browser, "input[id='search_form_input_homepage']")
+    verify_element_presence(browser, "input[id='search_button_homepage']")
+    browser.check_in_title("DuckDuckGo")
 
 
 @allure.story("Yandex")
 def test_yandex(browser):
-    browser.open_page("https://ya.ru")
+    browser.open_page("https://ya.ru/")
     verify_element_presence(browser, "#text")
     browser.check_in_title("Яндекс")
